cdc64ea0a695dd66d2a93a3ce9c7369ba364e054
Danil <honddk99@gmail.com>
2024-11-10 02:18:27 +0500

    create vac

--- src/app-pages/(authorized)/vacancy/VacancyPage.tsx
+++ src/app-pages/(authorized)/vacancy/VacancyPage.tsx
@@ -3,8 +3,10 @@
 import { hasCookie } from 'cookies-next';
 import { useRouter } from 'next/navigation';
 import { VacancyAdditionalDetails } from '@/widgets/job/ui';
-import { JobVacancyRespond, JobCreate } from '@/features/job/ui';
+import { JobVacancyRespond } from '@/features/job/ui';
 import { useGetJob } from '@/entities/job/hooks/useGetJob';
+import { useGetJobCandidates } from '@/entities/job-candidate/hooks/useGetJobCandidates';
+import { ResumeCard } from '@/entities/job-candidate/ui';
 import { Button } from '@/shared/ui/button';
 import { Skeleton } from '@/shared/ui/skeleton';
 
@@ -15,8 +17,27 @@ type TProps = {
 export function VacancyPage(props: TProps) {
   const { id } = props;
   const router = useRouter();
-  const { data, isPending } = useGetJob(Number(id));
   const isAuth = hasCookie('authToken')
+  const { data, isPending } = useGetJob(Number(id));
+
+  const { data: candidateData, isPending: candidateLoading } = useGetJobCandidates({
+    skills: data?.skills.map((skill) => skill.id),
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    cities: data?.city.id,
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    countries: data?.country.id,
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    sub_work_directions: data?.sub_work_direction.id,
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    work_direction: data?.work_direction.id,
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    work_experiences: data?.work_direction.id
+  })
 
   if (isPending) {
     return <Skeleton className="h-full w-full" />;
@@ -28,50 +49,59 @@ export function VacancyPage(props: TProps) {
 
   const { company, city, country, description_ru, modified, created } = data;
 
-  return (
+  return <>
     <div className="w-full px-4 py-4 mt-4 flex flex-col gap-6 lg:flex-row lg:gap-8">
-      <div className="lg:w-2/3">
-        <Button onClick={() => router.back()} variant={'outline'} className="mb-4">
+      <div className='lg:w-2/3'>
+        <Button onClick={() => router.back()} variant={'outline'} className='mb-4'>
           Назад
         </Button>
-        <div className="flex flex-col gap-2 sm:flex-row sm:gap-4">
-          <h1 className="text-2xl font-extrabold tracking-tight sm:text-3xl lg:text-4xl mb-4">
+        <div className='flex flex-col gap-2 sm:flex-row sm:gap-4'>
+          <h1 className='text-2xl font-extrabold tracking-tight sm:text-3xl lg:text-4xl mb-4'>
             Вакансия ({data?.name_ru})
           </h1>
         </div>
-        <div className="mb-6">
-          <h2 className="text-xl font-semibold tracking-tight sm:text-2xl">
+        <div className='mb-6'>
+          <h2 className='text-xl font-semibold tracking-tight sm:text-2xl'>
             {company.name}
           </h2>
-          <p className="text-gray-500">
+          <p className='text-gray-500'>
             {city.name_ru}, {country.name_ru}
           </p>
         </div>
-        <div className="mb-6">
-          <h3 className="text-xl font-semibold tracking-tight mb-2">
+        <div className='mb-6'>
+          <h3 className='text-xl font-semibold tracking-tight mb-2'>
             Описание
           </h3>
-          <p className="text-gray-700 text-sm sm:text-base">{description_ru}</p>
+          <p className='text-gray-700 text-sm sm:text-base'>{description_ru}</p>
         </div>
-        <div className="text-gray-400 text-xs sm:text-sm">
+        <div className='text-gray-400 text-xs sm:text-sm'>
           <p>Создано: {new Date(created).toLocaleDateString()}</p>
           <p>Обновлено: {new Date(modified).toLocaleDateString()}</p>
         </div>
         {!isAuth &&
-        <div className="hidden lg:block mt-6">
-          <h4 className="text-xl font-semibold tracking-tight mb-3">
-            Расскажите нам о себе
-          </h4>
-          <JobVacancyRespond jobId={Number(id)} />
-        </div>}
+          <div className='hidden lg:block mt-6'>
+            <h4 className='text-xl font-semibold tracking-tight mb-3'>
+              Расскажите нам о себе
+            </h4>
+            <JobVacancyRespond jobId={Number(id)}/>
+          </div>}
       </div>
-      <VacancyAdditionalDetails data={data} />
-      <div className="lg:hidden order-last w-full mt-6">
-        <h4 className="text-xl font-semibold tracking-tight mb-3">
+      <VacancyAdditionalDetails data={data}/>
+      {!isAuth &&
+      <div className='lg:hidden order-last w-full mt-6'>
+        <h4 className='text-xl font-semibold tracking-tight mb-3'>
           Расскажите нам о себе
         </h4>
-        <JobVacancyRespond jobId={Number(id)} />
-      </div>
+        <JobVacancyRespond jobId={Number(id)}/>
+      </div>}
     </div>
-  );
+    <div className="mt-6 flex overflow-x-auto space-x-6">
+      {candidateLoading && <p className="text-center mt-4">Загрузка...</p>}
+      {candidateData?.map((resume) => (
+        <div className="min-w-[200px]" key={resume.id}>
+          <ResumeCard data={resume}/>
+        </div>
+      ))}
+    </div>
+  </>
 }
--- src/entities/job-candidate/ui/ResumeCard/ResumeCard.tsx
+++ src/entities/job-candidate/ui/ResumeCard/ResumeCard.tsx
@@ -57,7 +57,7 @@ export function ResumeCard(props: TProps) {
       {data.cv_file && (
         <div className="mt-3">
           <a
-            className="bg-green-500 text-white text-sm font-medium px-4 py-2 rounded hover:bg-green-700 transition-colors duration-200 inline-block text-center"
+            className="bg-green-500 text-white text-sm font-medium px-4 py-2 rounded hover:bg-green-700 transition-colors duration-200 inline-block text-center mt-auto"
             rel="noopener noreferrer"
             href={data.cv_file}
             target="_blank"
