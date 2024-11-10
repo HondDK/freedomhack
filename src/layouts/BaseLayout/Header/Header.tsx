6f9db1a6734a8c2b7237464623bbe875874ffb59
Danil <honddk99@gmail.com>
2024-11-10 14:40:11 +0500

    create vac

--- src/layouts/BaseLayout/Header/Header.tsx
+++ src/layouts/BaseLayout/Header/Header.tsx
@@ -1,25 +1,22 @@
-'use client'
+'use client';
 
 import { deleteCookie, hasCookie } from 'cookies-next';
-import {
-  CircleUser,
-  Menu
-} from 'lucide-react';
-import { ScanEye } from 'lucide-react';
+import { CircleUser, Menu, ScanEye } from 'lucide-react';
 import Link from 'next/link';
 import { useLanguage } from '@/core/providers/I18nextProvider/LanguageContext/LanguageContext';
 import { Button } from '@/shared/ui/button';
 import {
-
   DropdownMenuContent,
   DropdownMenuTrigger,
   DropdownMenuItem,
   DropdownMenu,
-} from '@/shared/ui/dropdown-menu'
+} from '@/shared/ui/dropdown-menu';
 import { SelectContent, SelectTrigger, SelectValue, SelectItem, Select } from '@/shared/ui/select';
-import { SheetContent, SheetTrigger, Sheet } from '@/shared/ui/sheet'
+import { SheetContent, SheetTrigger, Sheet } from '@/shared/ui/sheet';
+import { useScopedI18n } from '@/shared/config';
 
 export function Header() {
+  const t = useScopedI18n('base.header');
   const { language, setLanguage } = useLanguage();
   const isAuth = hasCookie('authToken');
 
@@ -27,122 +24,96 @@ export function Header() {
     setLanguage(value);
   };
 
-  return <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-    <nav
-      className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-      <Link
-        className="flex items-center gap-2 text-lg font-semibold md:text-base"
-        href="#"
-      >
-        <ScanEye className="h-6 w-6"/>
-      </Link>
-      <Link
-        className="text-foreground transition-colors hover:text-foreground"
-        href="/"
-      >
-        Вакансии
-      </Link>
-      {isAuth && <>
-        <Link
-          className="text-muted-foreground transition-colors hover:text-foreground"
-          href="/companies"
-        >
-        Компании
+  return (
+    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+      <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+        <Link className="flex items-center gap-2 text-lg font-semibold md:text-base" href="#">
+          <ScanEye className="h-6 w-6" />
         </Link>
-        <Link
-          className="text-muted-foreground transition-colors hover:text-foreground"
-          href="/departaments"
-        >
-        Департаменты
+        <Link className="text-foreground transition-colors hover:text-foreground" href="/">
+          {t('vacancies')}
         </Link>
-        <Link
-          className="text-muted-foreground transition-colors hover:text-foreground"
-          href="/resume"
-        >
-        База резюме
-        </Link>
-      </>}
-    </nav>
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button
-          className="shrink-0 md:hidden"
-          variant="outline"
-          size="icon"
-        >
-          <Menu className="h-5 w-5"/>
-          <span className="sr-only">Toggle navigation menu</span>
-        </Button>
-      </SheetTrigger>
-      <SheetContent side="left">
-        <nav className="grid gap-6 text-lg font-medium">
-          <Link
-            className="flex items-center gap-2 text-lg font-semibold md:text-base"
-            href="#"
-          >
-            <ScanEye className="h-6 w-6"/>
-          </Link>
-          <Link
-            className="text-foreground transition-colors hover:text-foreground"
-            href="/"
-          >
-            Вакансии
-          </Link>
-          {isAuth && <>
-            <Link
-              className="text-muted-foreground transition-colors hover:text-foreground"
-              href="/companies"
-            >
-            Компании
+        {isAuth && (
+          <>
+            <Link className="text-muted-foreground transition-colors hover:text-foreground" href="/companies">
+              {t('companies')}
             </Link>
-            <Link
-              className="text-muted-foreground transition-colors hover:text-foreground"
-              href="/departaments"
-            >
-            Департаменты
+            <Link className="text-muted-foreground transition-colors hover:text-foreground" href="/departaments">
+              {t('departaments')}
             </Link>
-            <Link
-              className="text-muted-foreground transition-colors hover:text-foreground"
-              href="/resume"
-            >
-              База резюме
+            <Link className="text-muted-foreground transition-colors hover:text-foreground" href="/resume">
+              {t('resume_database')}
             </Link>
-          </>}
-        </nav>
-      </SheetContent>
-    </Sheet>
-    <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-      <form className="ml-auto flex-1 sm:flex-initial">
-      </form>
-      <Select onValueChange={handleLanguageChange} value={language}>
-        <SelectTrigger className="w-32">
-          <SelectValue placeholder="Select Language" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="ru">Русский</SelectItem>
-          <SelectItem value="kz">Қазақ</SelectItem>
-          <SelectItem value="en">English</SelectItem>
-        </SelectContent>
-      </Select>
-      {!isAuth && <Link className={'text-muted-foreground transition-colors hover:text-foreground'} href={'/auth'}>
-        Войти
-      </Link>}
-      {isAuth &&
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button className="rounded-full" variant="secondary" size="icon">
-            <CircleUser className="h-5 w-5"/>
-            <span className="sr-only">Профиль</span>
+          </>
+        )}
+      </nav>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button className="shrink-0 md:hidden" variant="outline" size="icon">
+            <Menu className="h-5 w-5" />
+            <span className="sr-only">{t('toggle_navigation')}</span>
           </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          {/*<DropdownMenuLabel>Мой профиль</DropdownMenuLabel>*/}
-          {/*<DropdownMenuSeparator/>*/}
-          {/*<DropdownMenuSeparator/>*/}
-          <DropdownMenuItem onClick={() => deleteCookie('authToken')}>Выйти с аккаунта</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      }
-    </div>
-  </header>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <nav className="grid gap-6 text-lg font-medium">
+            <Link className="flex items-center gap-2 text-lg font-semibold md:text-base" href="#">
+              <ScanEye className="h-6 w-6" />
+            </Link>
+            <Link className="text-foreground transition-colors hover:text-foreground" href="/">
+              {t('vacancies')}
+            </Link>
+            {isAuth && (
+              <>
+                <Link className="text-muted-foreground transition-colors hover:text-foreground" href="/companies">
+                  {t('companies')}
+                </Link>
+                <Link className="text-muted-foreground transition-colors hover:text-foreground" href="/departaments">
+                  {t('departaments')}
+                </Link>
+                <Link className="text-muted-foreground transition-colors hover:text-foreground" href="/resume">
+                  {t('resume_database')}
+                </Link>
+              </>
+            )}
+          </nav>
+        </SheetContent>
+      </Sheet>
+      <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+        <form className="ml-auto flex-1 sm:flex-initial" />
+
+        {/* Language Selection Dropdown */}
+        <Select onValueChange={handleLanguageChange} value={language}>
+          <SelectTrigger className="w-32">
+            <SelectValue placeholder={t('select_language')} />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="ru">{t('russian')}</SelectItem>
+            <SelectItem value="kz">{t('kazakh')}</SelectItem>
+            <SelectItem value="en">{t('english')}</SelectItem>
+          </SelectContent>
+        </Select>
+
+        {!isAuth && (
+          <Link className="text-muted-foreground transition-colors hover:text-foreground" href="/auth">
+            {t('login')}
+          </Link>
+        )}
+        {isAuth && (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button className="rounded-full" variant="secondary" size="icon">
+                <CircleUser className="h-5 w-5" />
+                <span className="sr-only">{t('profile')}</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem onClick={() => deleteCookie('authToken')}>
+                {t('logout')}
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
+      </div>
+    </header>
+  );
 }
