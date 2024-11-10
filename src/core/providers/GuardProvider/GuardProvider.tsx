a202bdd684ed55296e56a13b0b472ba7aec03b07
honddk <honddk99@gmail.com>
2024-11-10 16:19:31 +0500

    fix build

--- src/core/providers/GuardProvider/GuardProvider.tsx
+++ src/core/providers/GuardProvider/GuardProvider.tsx
@@ -3,7 +3,6 @@
 import { getCookie } from 'cookies-next';
 import { usePathname, useRouter } from 'next/navigation';
 import { PropsWithChildren, useEffect } from 'react';
-import { LoadingSpinner } from '@/shared/ui/LoadingSpiner';
 
 export default function GuardProvider(props: PropsWithChildren) {
   const { children } = props;
@@ -25,14 +24,6 @@ export default function GuardProvider(props: PropsWithChildren) {
       router.push('/auth');
     }
   }, [token, pathname, router]);
-
-  const condition =
-    (token && (pathname === '/auth' || pathname === '/registration')) ||
-    (!token && pathname !== '/auth' && pathname !== '/registration' && pathname !== '/' && !pathname.startsWith('/vacancy'));
-
-  if (condition) {
-    return <LoadingSpinner />;
-  }
 
   return <>{children}</>;
 }
